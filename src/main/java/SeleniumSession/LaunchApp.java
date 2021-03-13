b69a288bb42d914d3e91336a1970e1312a8461cc
samarth2503 <66519250+samarth2503@users.noreply.github.com>
2021-03-13 18:16:15 +0530

    Create LaunchApp.java

---
+++ src/main/java/SeleniumSession/LaunchApp.java
@@ -0,0 +1,15 @@
+package SeleniumSession;
+
+public class LaunchApp {
+	
+	public static void main(String[] args)
+	{
+		System.out.println("remote--main method");
+	}
+	
+	public void login()
+	{
+		System.out.println("remote---Login method");
+	}
+
+}
