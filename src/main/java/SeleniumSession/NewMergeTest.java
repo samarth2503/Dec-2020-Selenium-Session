6f7746ff8d685f4a9e4efe47f858cb4418574470
samarth2503 <66519250+samarth2503@users.noreply.github.com>
2021-03-13 19:11:19 +0530

    Create NewMergeTest.java

---
+++ src/main/java/SeleniumSession/NewMergeTest.java
@@ -0,0 +1,21 @@
+package SeleniumSession;
+
+public class NewMergeTest {
+	
+	public void login()
+	{
+		System.out.println("Remote---Login");
+	}
+	
+	public void logout()
+	{
+		System.out.println("Remote--logout");
+	}
+	
+	public void merge()
+	{
+		System.out.println("Remote--merge");
+	}
+
+
+}
