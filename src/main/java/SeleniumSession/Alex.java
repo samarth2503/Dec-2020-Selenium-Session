8dbea2e8b3a04abe2acb06686ae1cf8cbd695a84
samarth2503 <66519250+samarth2503@users.noreply.github.com>
2021-03-24 21:54:38 +0530

    Create Alex.java
    This alex class file

---
+++ src/main/java/SeleniumSession/Alex.java
@@ -0,0 +1,16 @@
+package SeleniumSession;
+
+public class Alex {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This Alex class");
+		
+
+	}
+  
+  public static void m1()
+  {
+    System.out.println("This m1 file");
+  }
+}
