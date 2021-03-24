0d59e8742c613704b1b0ac1dec58da450f606974
samarth2503 <66519250+samarth2503@users.noreply.github.com>
2021-03-24 23:06:27 +0530

    Create NewMergeTest.java
    Add m1 Method to New MergeTest

---
+++ src/main/java/SeleniumSession/NewMergeTest.java
@@ -0,0 +1,16 @@
+package SeleniumSession;
+
+public class NewMergeTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This newMergeTest class");
+	
+	}
+  
+  public static void m1()
+  {
+    System.out.println("This is m1 Method");
+  }
+
+}
